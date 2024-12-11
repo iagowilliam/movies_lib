fd806e4106b279b9ae5304e44fd4c52abf74dda1
Iago William <iagowilliam@hotmail.com>
2024-12-11 15:43:52 -0300

    feito o componente navbar e as estilizações iniciais do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import './App.css'
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
-    <nav id='navbar'>
-      <h2>
-        <Link to="/">MoviesLib</Link>
-      </h2>
-      <Link to="/movie/1">Movie</Link>
-      <Link to="/search">Search</Link>
-    </nav>
+      <Navbar />
       <h2>Movies Lib</h2>
       <Outlet />
     </>
