3cdaeca7992e025a8a62121c2c06d9ead32d4cbd
Iago William <iagowilliam@hotmail.com>
2024-12-11 14:57:35 -0300

    Configurado o router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    <nav id='navbar'>
+      <h2>
+        <Link to="/">MoviesLib</Link>
+      </h2>
+      <Link to="/movie/1">Movie</Link>
+      <Link to="/search">Search</Link>
+    </nav>
       <h2>Movies Lib</h2>
+      <Outlet />
     </>
   )
 }
